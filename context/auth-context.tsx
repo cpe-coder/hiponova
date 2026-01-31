240360e9667977774bca4766c389a615af95e5cb
Grian Gajila <griangajila2@gmail.com>
2026-01-31 22:05:58 +0800

    wip

--- context/auth-context.tsx
+++ context/auth-context.tsx
@@ -12,8 +12,7 @@ interface AuthProps {
 }
 
 const TOKEN_KEY = "TOKEN";
-export const API_URL = "https://ripe-sensei-server-9z7p.vercel.app";
-// export const API_URL = "http://localhost:3000";
+export const API_URL = "https://hiponova-server.vercel.app";
 const AuthContext = createContext<AuthProps>({});
 
 export const useAuth = () => {
@@ -50,9 +49,8 @@ export const AuthProvider = ({ children }: any) => {
 		const verifyToken = async () => {
 			const token = await SecureStore.getItemAsync(TOKEN_KEY);
 
-			const authorization = (axios.defaults.headers.common[
-				"Authorization"
-			] = `Bearer ${token}`);
+			const authorization = (axios.defaults.headers.common["Authorization"] =
+				`Bearer ${token}`);
 
 			try {
 				await axios
@@ -99,9 +97,8 @@ export const AuthProvider = ({ children }: any) => {
 			token: result.data.token,
 			authenticated: true,
 		});
-		axios.defaults.headers.common[
-			"Authorization"
-		] = `Bearer ${result.data.token}`;
+		axios.defaults.headers.common["Authorization"] =
+			`Bearer ${result.data.token}`;
 
 		await SecureStore.setItemAsync(TOKEN_KEY, result.data.token);
 
